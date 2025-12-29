1a988165d2abbbe66cd139994453e0fb23dd2e12
Alyssa Evans <alyda@me.com>
2025-12-29 05:34:10 +0000

    Add inline blame for jujutsu
    GitLens is too heavy

--- shell.nix
+++ shell.nix
@@ -2,9 +2,4 @@
 
 pkgs.mkShell {
   buildInputs = with pkgs; [ just cheat ];
-  
-  # shellHook = ''
-  #   jj config set --user user.name "$(git config --get user.name)" 
-  #   jj config set --user user.email "$(git config --get user.email)"
-  # '';
 }
